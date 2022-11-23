11864a7672e8f68c3c31afc7d901d2e38da64a06
Joshua Mathew <umjoshua@gmail.com>
2022-11-23 07:29:26 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -14,13 +14,16 @@ dotenv.config();
 const PORT = process.env.PORT;
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zevvmfz.mongodb.net/test`;
 app.use(express.json())
-app.use('/events', eventRoutes);
-app.use('/admin',adminRoutes);
-
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+}
 app.use(cors());
+app.use('/events', eventRoutes);
+app.use('/admin', adminRoutes);
 
 
 mongoose.connect(CONNECTION_URL).then(() => {
