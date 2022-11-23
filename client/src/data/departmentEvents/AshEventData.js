ceb0452e5c0eab44f834616279e05d0e3117399c
antonythomas123 <antonythomas993@gmail.com>
2022-11-23 22:30:33 +0530

    Department Data updated

--- client/src/data/departmentEvents/AshEventData.js
+++ client/src/data/departmentEvents/AshEventData.js
@@ -3,13 +3,23 @@ const AshEventData = [
         name: 'Project Incubation',
         description:'Experiments related to physics ',
         event_type: 'Competition',
-        fee: 'FREE'
+        fee: 'FREE',
+        student1: '',
+        s1_phone: '',
+        student2: '',
+        s2_phone: '',
+        staff: 'Mr.Jomy Jose',
     },
     {
         name: 'Chaos Game',
         description: 'Mathematical Game',
         event_type: 'Games',
-        fee: 'FREE'
+        fee: 'FREE',
+        student1: '',
+        s1_phone: '',
+        student2: '',
+        s2_phone: '',
+        staff: 'Ms.Divya Thomas ',
     }
 ];
 
