12501ee84aa08eb48da09fd3cbd2a32cc814733b
Joshua Mathew <umjoshua@gmail.com>
2022-11-23 23:29:36 +0530

    registration page

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Home from './pages/home/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MechanicalEvents from './components/EventListCards/MechanicalEvents/MechanicalEvents';
+import Register from './pages/register/Register';
+import ThanksPage from './pages/register/thanks';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path = '/' element={<Home/>}/>
-        <Route exact path = '/me-events' element={<MechanicalEvents/>}/>
-        <Route exact path = '/cse-events' element={<MechanicalEvents/>}/>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/me-events' element={<MechanicalEvents />} />
+        <Route exact path='/cse-events' element={<MechanicalEvents />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/thanks' element={<ThanksPage />} />
       </Routes>
     </Router>
   )
---
+++ client/src/pages/register/thanks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import tickIcon from '../../assets/icons/tick.png'
+import './style.css';
+
+function ThanksPage() {
+    return (
+        <div >
+            <div className='container flex flex-col justify-center items-center'>
+                <img src={tickIcon}/>
+                <h1>Thankyou for registering </h1>
+                <button className='mt-3'>Return to Home</button>
+            </div>
+        </div>
+    )
+}
+
+export default ThanksPage
